01aa778884f8c56d30db62f3679886ce7bdc325f
lucylufei <lucylulu97@hotmail.com>
2017-08-20 20:50:52 -0600

    reorganize

--- default.js
+++ default.js
@@ -23,8 +23,21 @@ function displaySearch() {
     document.body.appendChild(x);
 }
 
+
 function hide_divs(search) {
-    $("#container > div").hide(); // hide all divs
-	$('#container > div[id*="'+search+'"]').show(); // show the ones that match
+	if (search != "") {
+		$("#container > div").hide(); // hide all divs
+		$('#container > div[id*="'+search+'"]').show(); // show the ones that match
+	}
+	else
+		$("#container > div").show(); // show all divs
+	
 }
 
+function clear_search() {
+    $("#container > div").show(); // show all divs
+}
+
+function hide_search(){
+	$("#search_field").hide();
+}
---
+++ jQ_default.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+ /*   $("#searchIcon").click(function(){
+        $("#search_field").toggle();
+		document.getElementById("search_field").value = "";
+		clear_search();
+	}); */
+	$("#search_field").keyup(function() {
+		var search = $.trim(this.value);
+		hide_divs(search);
+	});
+	
+});
+$(document).click(function(e) {   
+    if(e.target.id == "searchIcon") {
+        $("#search_field").toggle();
+		document.getElementById("search_field").value = "";
+		clear_search();
+	}
+	else if (e.target.id == "search_div" || e.target.id == "search_field") {
+	}
+	else {
+        hide_search();
+		clear_search();
+		document.getElementById("search_field").value = "";
+    } 
+});
