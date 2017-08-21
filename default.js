96be2163b407a8a0dd2b400005ecfd764e99da82
lucylufei <lucylulu97@hotmail.com>
2017-08-20 20:40:57 -0600

    search <3

--- default.js
+++ default.js
@@ -16,3 +16,15 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+
+function displaySearch() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "search");
+    document.body.appendChild(x);
+}
+
+function hide_divs(search) {
+    $("#container > div").hide(); // hide all divs
+	$('#container > div[id*="'+search+'"]').show(); // show the ones that match
+}
+
