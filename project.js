8c55b725a535c8bf963978ad3fd4943b8f8df3b5
lucylufei <lucylufei@users.noreply.github.com>
2019-03-15 08:10:09 -0400

    template(pt.1)

---
+++ project.js
@@ -0,0 +1,78 @@
+var tags = [];
+var skills = [];
+// Populate all fields
+$.getJSON("http://www.lufei.ca/projects.json", function(data){
+    $.each(data.projects, function(id, project){
+    	var tile = "<div class=\"tile col-sm-6 col-lg-4 col-xl-3";
+    	for (i = 0; i < project.tags.length; i++){
+    		tile += " " + project.tags[i];
+    	}
+    	tile += "\" id=\"";
+    	tile += project.id;
+    	tile += "\">"
+
+    	tile += "<div class=\"card\">"
+
+
+        tile += "<h1 class=\"card-title\">" + project.title + "</h1>";
+
+
+        tile += "</div></div>";
+        $("#content_wrapper").append(tile);
+    });
+});
+
+// Generate tag array
+$.getJSON("http://www.lufei.ca/projects.json", function(data){
+	// iterate through all projects
+	$.each(data.projects, function(id, project){
+		var i;
+		// iterate through all tags
+		for (i = 0; i < project.tags.length; i++){
+			if (tags.includes(project.tags[i]) == false){
+				// add to list if not found
+				tags.push(project.tags[i]);
+			}
+		}
+	});
+	
+	// generate buttons for each tag
+	$.each(tags, function(i, tag){
+		var tag_btn = "<button class=\"filterbtn btn\" id=\"" + tag + "\">" + tag + "</button>";
+		$("#filters").append(tag_btn);
+	});
+
+	// filter according to tag
+	$(".filterbtn").click(function(){
+		var tag_selected = this.id;
+		$(".tile").hide();
+		$(".tile").filter('.' + tag_selected).show();
+
+		$("#All").removeClass("clicked_btn");
+		$(".filterbtn").removeClass("clicked_btn");
+		$(this).addClass("clicked_btn");
+	});
+});
+
+
+// Generate skills array
+$.getJSON("http://www.lufei.ca/projects.json", function(data){
+	$.each(data.projects, function(id, project){
+		var i;
+		for (i = 0; i < project.skills.length; i++){
+			if (skills.includes(project.skills[i]) == false){
+				skills.push(project.skills[i]);
+			}
+		}
+	});
+});
+
+
+$(document).ready(function(){
+	$("#All").click(function(){
+		$(".tile").show();
+		$(this).addClass("clicked_btn");
+		$(".filterbtn").removeClass("clicked_btn");
+	});
+});
+
