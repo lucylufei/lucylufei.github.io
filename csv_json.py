9f1fdb540073f1ce2101630ada4bc6f0089aa137
lucylufei <lucylufei@users.noreply.github.com>
2019-03-16 23:34:36 -0400

    csv-json generate project

---
+++ csv_json.py
@@ -0,0 +1,39 @@
+import csv
+
+path = r"C:\Users\lucyl\Documents\GitHub\lucylufei.github.io\projectscsv.csv"
+path_json = r"C:\Users\lucyl\Documents\GitHub\lucylufei.github.io\project.json"
+
+csvfile = open(path, newline='') 
+	
+projects = csv.reader(csvfile, delimiter=',')
+
+keys = []
+project_array = []
+
+for row in projects:
+	project_array.append(row)
+
+for key in project_array[0]:
+	keys.append(key)
+
+print(keys)
+
+
+jsonfile = open(path_json, "w+")
+
+jsonfile.write("{\"projects\":[")
+
+for project in range(1, len(project_array)):
+	jsonfile.write("{\n")
+	for i, key in enumerate(keys):
+		jsonfile.write("\"{key}\":".format(key=key))
+		if "tags" in key or "skills" in key:
+			jsonfile.write("[\"{data}\"]".format(data=project_array[project][i].replace(",", "\",\"")))
+		else:
+			jsonfile.write("\"{data}\"".format(data=project_array[project][i]))
+		if i is not len(keys)-1:
+			jsonfile.write(",\n")
+	jsonfile.write("}\n")
+
+jsonfile.write("]}")
+jsonfile.close()
