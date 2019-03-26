f2eeb98106d4417244710972fd641301b14a40ae
lucylufei <lucylufei@users.noreply.github.com>
2019-03-25 21:40:33 -0400

    button +  footer

--- project.js
+++ project.js
@@ -85,3 +85,19 @@ $(document).ready(function(){
 	});
 });
 
+window.onscroll = function(){
+	scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("topbtn").style.display = "block";
+    } else {
+        document.getElementById("topbtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
