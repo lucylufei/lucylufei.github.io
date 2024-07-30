eb372e7a691ec94c741dc7b2478c21e3edce6d62
LuFei Liu <lucylufei@users.noreply.github.com>
2024-07-29 21:05:50 -0700

    hike log update :)

--- hikelog/generate_hike_log.py
+++ hikelog/generate_hike_log.py
@@ -1,12 +1,5 @@
 f = open("hikes.csv", "r")
 
-ordered_regions = [
-    "Lower Mainland",
-    "Fraser Valley",
-    "Sea to Sky",
-    "Rockies",
-]
-
 dataset = {}
 regions = set()
 for line in f.readlines()[1:]:
@@ -16,28 +9,57 @@ for line in f.readlines()[1:]:
         "region" : content[1],
         "rating" : content[3],
         "rec" : content[4],
-        "comment" : ", ".join(content[5:]).replace('"', '')
+        "year": content[5],
+        "comment" : ", ".join(content[6:]).replace('"', ''),
     }
     regions.add(content[1])
 
 f.close()
 
-def create_card(region, dataset):
-    region_class = region.replace(" ", "-").replace("(", "").replace(")", "").lower()
-    f.write('<div class="card region" id="{c}"><div class="card-header"><table><tr><th style="text-align: left; width: 100vw;">{r}</th><th style="text-align: right;"><i class="fa fa-sort-down"></i></th></tr></table></div></div></br>\n'.format(r=region, c=region_class))
-    f.write('<div class="card-columns">\n')
-    for hike in dataset:
+def create_cards(dataset):
+    hikes_list = sorted(dataset.keys())
+    # hikes_list.sort(key=lambda x: dataset[x]["done"] == "Yes", reverse=True)
+
+    for hike in hikes_list:
+        print(hike)
+
         hike_done = (dataset[hike]["done"] == "Yes")
         num_stars = int(dataset[hike]["rec"]) - 2 if hike_done else 0
-        if dataset[hike]["region"] == region:
-            f.write('<div class="card {region} rate_{rating} {done}">'.format(region=region_class, rating=str(num_stars), done="done" if hike_done else ""))
-            if hike_done:
-                f.write('<img src="./images/hikes/{img}.JPG" class="card-img-top" alt="...">'.format(img=hike.replace(" ", "-").replace("'", "").lower()))
-                star_rating = ' <i class="fa fa-heart" aria-hidden="true"></i>' * num_stars
-                star_rating += ' <i class="fa fa-heart-o" aria-hidden="true"></i>' * (3 - num_stars)
-                f.write('<div class="card-body"><h5 class="card-title">{name}</h5><p class="card-text">{rating}</br>{rec}<hr>{comment}</p></div></div>\n'.format(name=hike, rating=dataset[hike]["rating"], rec=star_rating, comment=dataset[hike]["comment"]))
-            else:
-                f.write('<div class="card-body"><h5 class="card-title" style="color: gray;">{name}</h5></div></div>\n'.format(name=hike, rating=dataset[hike]["rating"]))
+        region = dataset[hike]["region"]
+        year = 0 if dataset[hike]["year"] == "" else int(dataset[hike]["year"])
+        region_class = region.replace(" ", "-").replace("'", "").lower()
+        
+        if dataset[hike]["rating"] == "Easy":
+            diff = "1"
+        elif dataset[hike]["rating"] == "Moderate":
+            diff = "2"
+        elif dataset[hike]["rating"] == "Hard":
+            diff = "3"
+        else:
+            diff = "4"
+
+        f.write('<div class="card rate_{rating} {done}" data-name={name} data-region={region} data-rating={rating} data-est-year={year} data-difficulty={diff}>'.format(region=region_class, rating=str(num_stars), done="done" if hike_done else "", name=hike.replace(" ", "-").replace("'", "").lower(), year=year, diff=diff))
+        if hike_done:
+            # Add image
+            f.write('<img src="./images/hikes/{img}.JPG" class="card-img-top" alt="...">'.format(img=hike.replace(" ", "-").replace("'", "").lower()))
+
+            # Calculate star rating
+            star_rating = ' <i class="fa fa-heart" aria-hidden="true"></i>' * num_stars
+            star_rating += ' <i class="fa fa-heart-o" aria-hidden="true"></i>' * (3 - num_stars)
+
+            # Add badges
+            year_badge = '<span class="highlight est-year">{year}</span>'.format(year=year)
+            rating_badge = '<span class="highlight rating">{rating}</span>'.format(rating=star_rating)
+            difficulty_badge = '<span class="highlight difficulty-{rate}">{rating}</span>'.format(rate=dataset[hike]["rating"].lower() , rating=dataset[hike]["rating"])
+            region_badge = '<span class="highlight region">{region}</span>'.format(region=region)
+
+            badges = rating_badge + difficulty_badge + region_badge + year_badge
+
+            # Make card
+            f.write('<div class="card-body"><h5 class="card-title">{name}</h5><p class="card-text">{comment}</p>{badges}</div></div>\n'.format(name=hike, comment=dataset[hike]["comment"], badges=badges))
+        else:
+            region_badge = '<span class="highlight region">{region}</span>'.format(region=region)
+            f.write('<div class="card-body"><h5 class="card-title" style="color: gray;">{name}</h5>{badges}</div></div>\n'.format(name=hike, rating=dataset[hike]["rating"], badges=region_badge))
 
     f.write("</div>")
 
@@ -52,7 +74,7 @@ f.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="
 f.write("<script async src='https://www.googletagmanager.com/gtag/js?id=UA-102991991-2'></script><script>window.dataLayer = window.dataLayer || [];function gtag() {dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-102991991-2'); </script>\n\n")
 
 # Files
-f.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet"><link rel="icon" href="./images/L.png"><link rel="stylesheet" href="common.css"><link rel="stylesheet" href="lululetter.css"><link rel="stylesheet" href="topbutton.css"><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>\n\n')
+f.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet"><link rel="icon" href="./images/L.png"><link rel="stylesheet" href="common.css"><link rel="stylesheet" href="hikes.css"><link rel="stylesheet" href="topbutton.css"><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>\n\n')
 
 # Analytics
 f.write("<script>(function (i, s, o, g, r, a, m) {i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {(i[r].q = i[r].q || []).push(arguments)}, i[r].l = 1 * new Date(); a = s.createElement(o),m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m) })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); ga('create', 'UA-102991991-1', 'auto'); ga('send', 'pageview');</script>\n\n")
@@ -66,24 +88,23 @@ f.write("$('#topbtn').click(function () {jQuery('html,body').animate({scrollTop:
 f.write("$('.region').click(function () {var region = this.id; $('.' + region).animate({ height: 'toggle'});});\n")
 f.write("});</script>\n\n")
 
+f.write('<script src="hikes.js"></script>')
+
 f.write("</head>\n\n")
 
 # Body
 f.write("<body>\n")
 
 # Navbar
-f.write('<!-- Nav Bar --><nav class="navbar sticky-top navbar-expand-lg shadow-sm"><a class="navbar-brand" href="index.html" style="font-size: 2em;">Lufei</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarText"><ul class="navbar-nav mr-auto"><li class="nav-item"><a class="nav-link text-center" href="resume.html">Resume</a></li><li class="nav-item"><a class="nav-link text-center" href="project.html">Projects</a></li><li class="nav-item"><a class="nav-link text-center active" href="menu.html">Life</a></li><li class="nav-item"><a class="nav-link text-center" href="lululetter.html">lululetter</a></li></ul></div></nav>\n\n')
+f.write('<!-- Nav Bar --><nav class="navbar sticky-top navbar-expand-lg shadow-sm"><a class="navbar-brand" href="index.html" style="font-size: 2em;">Lufei</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarText"><ul class="navbar-nav mr-auto"><li class="nav-item"><a class="nav-link text-center" href="resume.html">Resume</a></li><li class="nav-item"><a class="nav-link text-center" href="project.html">Projects</a></li><li class="nav-item"><a class="nav-link text-center active" href="menu.html">Life</a></li><li class="nav-item"><a class="nav-link text-center" href="lululetter/index.html">lululetter</a></li></ul></div></nav>\n\n')
 
 f.write('<div class="container"><div class="header"><h1>Hike Log</h1></div>\n')
 
-f.write('<div class="faded" style="text-align: center;"><a href="https://www.instagram.com/propinquity.effect/" target="_blank">Questions? Ask <span class="special-link">@propinquity.effect</span></a></div><br>\n')
+f.write('<div class="controls"><form><div class="form-group"><label for="sorting">Sort by: </label><select id="sorting"><option value="name">Name</option><option value="region">Region</option><option value="difficulty">Difficulty</option><option value="rating">Rating</option><option value="est-year">Year</option></select></div><div class="form-group"><label for="filter">Filter: </label><select id="filter"><option value="all">All</option></select></div><div class="form-group"><input type="checkbox" id="toggle-done"><label for="toggle-done">To Dos</label></div></form></div>')
 
-for region in ordered_regions:
-    create_card(region, dataset)
-    regions.remove(region)
-
-for region in regions:
-    create_card(region, dataset)
+f.write("<div class='card-columns'>\n")
+create_cards(dataset)
+f.write("</div>\n")
 
 f.write('<button id="topbtn" style="margin:0 auto;">Back to Top</button>')
 
---
+++ hikes.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+    var vw = $(window).width();
+    var n_columns;
+    if (vw < 768) {
+        n_columns = Math.floor(vw / 200);
+    } else {
+        n_columns = Math.floor(vw / 300);
+    }
+    $('.card-columns').css('column-count', n_columns);
+});
+
+$(document).ready(function () {
+    // Get all the different regions in hikes
+    var regions = [];
+    var region_names = [];
+    $(".card").each(function () {
+        var region = $(this).data("region");
+        if (!regions.includes(region)) {
+            regions.push(region);
+
+            // Clean up region names
+            // Replace dashes with spaces
+            region_name = region.replace(/-/g, " ");
+            // Capitalize every first letter
+            region_name = region_name.replace(/\b\w/g, function (char) {
+                return char.toUpperCase();
+            });
+
+            // If BC is in the name, replace it with British Columbia
+            if (region_name.includes("Bc")) {
+                region_name = region_name.replace("Bc", "BC");
+            }
+
+            // Track region and region name together
+            region_names.push({region: region, region_name: region_name});
+        }
+    });
+
+    // Sort the regions
+    region_names.sort();
+
+    // Create the select box
+    var select = $("#filter");
+    region_names.forEach(function (region) {
+        select.append("<option value='" + region.region + "'>" + region.region_name + "</option>");
+    });
+});
+
+// Function
+function sort_hikes(hikes, selected, checked) {
+    console.log("Sorting hikes by " + selected + " and checked: " + checked);
+    // Filter hikes
+    hikes.each(function () {
+        var done = $(this).hasClass("done");
+        var region = $(this).data("region");
+
+        // Check for class done
+        if (checked && done) {
+            $(this).hide();
+        } else {
+            // Check filters
+            if (selected == "all") {
+                $(this).show();
+            } else if (region === selected) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        }
+    });
+}
+
+$(document).ready(function () {
+    // on change of the select box
+    $("#sorting").change(function () {
+        // get the selected value
+        var selected = $(this).val();
+        console.log("Sorting by " + selected);
+
+        // sort the hikes
+        var hikes = $(".card");
+
+        if (selected === "rating" || selected === "est-year") {
+            hikes.sort(function (a, b) {
+                var aVal = $(a).data(selected);
+                var bVal = $(b).data(selected);
+                
+                if (aVal < bVal) {
+                    return 1;
+                } else if (aVal > bVal) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+        }
+        else {
+            hikes.sort(function (a, b) {
+                var aVal = $(a).data(selected);
+                var bVal = $(b).data(selected);
+                
+                if (aVal < bVal) {
+                    return -1;
+                } else if (aVal > bVal) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+        }
+        // empty the container
+        $(".card-columns").empty();
+        // append the sorted hikes
+        hikes.each(function () {
+            $(".card-columns").append($(this));
+        });
+    });
+
+    $("#toggle-done").change(function () {
+        var checked = $(this).prop('checked');
+        var hikes = $(".card");
+        var selected = $("#filter").val();
+        console.log("Toggle done hikes: " + checked);
+        sort_hikes(hikes, selected, checked);
+    });
+
+    $("#filter").change(function () {
+        var hikes = $(".card");
+        var selected = $(this).val();
+        var checked = $("#toggle-done").prop('checked');
+        console.log("Filtering by " + selected);
+        sort_hikes(hikes, selected, checked);
+    });
+});
